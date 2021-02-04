ccff02215525b54dd118d49700bc3f6993a1ba40
karunakaushik <karunakaushik22800@gmail.com>
2021-02-04 16:56:42 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ let app = angular.module("Myapp", []);
 
 app.controller("Myctrl", ($scope, $http) => {
     // controller here......
-    $scope.title= "Stay Home Stay Safe Stay Healty";
+    $scope.title = "Stay Home Stay Safe Stay Healthy";
 
     // On change Function.........
     // $scope.changevalue= () => {
     //     $scope.title= " Bye People";
     // }
     $http.get(URL).then(
-        (response)=>{
+        (response) => {
             console.log("API LOADED")
             // console.log(response.data);
 
@@ -20,23 +20,23 @@ app.controller("Myctrl", ($scope, $http) => {
             // console.log(error);
         }
     );
-   //get_country_data
-   $scope.get_country_data = () => {
-       let country = $scope.c; 
-       if(country == ""){
-           $scope.c_data= undefined;
-           return;
-       }
+    //get_country_data
+    $scope.get_country_data = () => {
+        let country = $scope.c;
+        if (country == "") {
+            $scope.c_data = undefined;
+            return;
+        }
 
-       $http.get(`${URL}/countries/${country}`).then(
-           (response)=> {
-            //    console.log(response.data);
-               $scope.c_data = response.data;
-           },
-           (error) => {
-            //    console.log(error);
-           }
-       );
-   };
+        $http.get(`${URL}/countries/${country}`).then(
+            (response) => {
+                //    console.log(response.data);
+                $scope.c_data = response.data;
+            },
+            (error) => {
+                //    console.log(error);
+            }
+        );
+    };
 
 })
